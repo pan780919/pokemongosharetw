196a2b8f26bff8c4f80c4112925a4d8240d514b5
jackpan <jackpan@hyxen.com>
2016-08-10 14:00:56 +0800

    1. add news for uers 2. google play

--- app/src/main/java/com/jhengweipan/pokemongo/MenuListActivity.java
+++ app/src/main/java/com/jhengweipan/pokemongo/MenuListActivity.java
@@ -1,7 +1,9 @@
 package com.jhengweipan.pokemongo;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,6 +22,7 @@ public class MenuListActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_list);
         initLayout();
+
     }
 
     private void initLayout() {
@@ -27,6 +30,7 @@ public class MenuListActivity extends Activity {
         ArrayList<String> marraylist = new ArrayList<>();
         marraylist.add("玩家分享區");
         marraylist.add("玩家推送訊息平台");
+        marraylist.add("關於這個APP");
         ArrayAdapter<String> pageName = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, marraylist);
         mListview.setAdapter(pageName);
         mListview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -57,12 +61,35 @@ public class MenuListActivity extends Activity {
                         intent2.putExtras(bundle2);
                         //切換Activity
                         startActivity(intent2);
-
+                        break;
+                    case 2:
+                        showDilog();
                         break;
 
                 }
 
             }
         });
+    }
+    private void showDilog(){
+        new AlertDialog.Builder(this)
+                .setIcon(R.drawable.poke)
+                .setTitle("關於")
+                .setMessage("這個是類似共享的平台,玩家可以分享文章至blog,也可以用推播訊息 告知附近有什麼特別的pokemon\n\n" +
+                        "如有意願或興趣 可以加入我們\n\n" +
+                        "blog 開放申請 可以寫信告知會第一時間把您加入作者行列\n\n" +
+                        "推播訊息平台每個人都可以使用\n" +
+                        "帳號:pokemontw\n" +
+                        "密碼:pokemon123\n" +
+                        "請勿亂改密碼 以便大家使用 感謝!!"
+
+                )
+                .setNegativeButton("確定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                }).show();
+
     }
 }
